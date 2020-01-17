2cac4ded85443162e644ededce0fe83c87f16bd4
Paola <camachopaola311@gmail.com>
2020-01-17 10:12:18 -0500

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Paola Camacho on 1/16/20.
+//  Copyright © 2020 Paola Camacho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
